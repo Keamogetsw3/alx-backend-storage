55442a235504289ebe72aa083de2885d5fda62cc
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2024-11-21 11:35:38 +0200

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -5,7 +5,5 @@
 -- 3. Use the attributes `formed` and `split` to compute the lifespan.
 -- 4. The script should work on any database.
 
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
-    FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-    ORDER BY lifespan DESC;
+SELECT band_name, (IFNULL(split, 2022) - formed)
+AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
