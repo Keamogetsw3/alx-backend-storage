3a0cd04aa1d0f70e8527e7f0f1d06fbff3037aa2
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2025-01-31 15:30:56 +0200

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-'''A module that provides tools for caching HTTP requests and tracking request counts using Redis.'''
+'''A module that provides tools for caching HTTP requests
+and tracking request counts using Redis.'''
 
 import redis
 import requests
@@ -8,38 +9,42 @@ from typing import Callable
 
 
 redis_store = redis.Redis()
-'''A Redis client instance used for caching request results and tracking request counts.'''
+'''A Redis client instance used for caching request results
+and tracking request counts.'''
 
 
 def data_cacher(method: Callable) -> Callable:
-    '''A decorator that caches the response of an HTTP request and tracks the number of times a URL is requested.'''
-    
+    '''A decorator that caches the response of an HTTP request
+    and tracks the number of times a URL is requested.'''
+
     @wraps(method)
     def invoker(url: str) -> str:
-        '''Increments the request count, retrieves cached data if available, 
+        '''Increments the request count,
+        retrieves cached data if available, 
         or fetches and caches the response if not.
         '''
         # Increment the request count for the given URL
         redis_store.incr(f'count:{url}')
-        
+
         # Check if the response is already cached
         result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
-        
+
         # Fetch the response if not cached
         result = method(url)
-        
-        # Reset the request count and cache the response with a 10-second expiration
+
         redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
-        
+
         return result
-    
+
     return invoker
 
 
 @data_cacher
 def get_page(url: str) -> str:
-    '''Fetches the content of a given URL, caches the response, and tracks request counts.'''
+    '''Fetches the content of a given URL,
+    caches the response,
+    and tracks request counts.'''
     return requests.get(url).text
